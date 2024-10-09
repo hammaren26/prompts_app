a7b792661e09839cafb23c188ee23b46cd707e42
Vyacheslav <hammaren23@gmail.com>
2024-10-09 07:34:53 +0600

    убрал кеширование запросов

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -7,10 +7,20 @@ export const GET = async () => {
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    // Создаем объект ответа с заголовками
+    const response = new Response(JSON.stringify(prompts), { status: 200 });
+    response.headers.set("Cache-Control", "no-store"); // Отключаем кэширование
+
+    return response;
   } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-    });
+    const errorResponse = new Response(
+      JSON.stringify({ error: error.message }),
+      {
+        status: 500,
+      }
+    );
+    errorResponse.headers.set("Cache-Control", "no-store"); // Отключаем кэширование в случае ошибки
+
+    return errorResponse;
   }
 };
